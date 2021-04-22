f5b2f12cebdff9cf7010324b6873d6d0e58ccfe6
AhmedHajiyev <hajiyev.ahmed01@gmail.com>
2021-04-22 23:32:44 +0400

    jsde slider hissesini yigdim

--- Front-End/PortfolioProject/js/script.js
+++ Front-End/PortfolioProject/js/script.js
@@ -1,10 +1,14 @@
 /*let images = document.querySelectorAll('.slider');
 let start = 0;
+let dots = document.querySelectorAll('.dot')
+
 images[start].style.display = 'block'
 
+console.log(dots)
+
 const moveSlider = () => {
-    if (start == images.length - 1) {
-        start = -1;
+    if (start = 1) {
+        start = 0;
     };
     for (let i = 0; i < images.length; i++) {
         images[i].style.display = 'none'
@@ -12,7 +16,80 @@ const moveSlider = () => {
 
 }
 
+
+
 setInterval(() => {
     moveSlider();
 
+}, 2000)*/
+
+
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slider");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = n - 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", " ");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+
+    console.log(slides)
+
+}
+
+setInterval(() => {
+    showSlides();
+
 }, 2000)
+
+console.log(showSlides)
+
+/*<div class="slideshow-container">
+
+<div class="mySlides fade">
+  <div class="numbertext">1 / 3</div>
+  <img src="img_nature_wide.jpg" style="width:100%">
+  <div class="text">Caption Text</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">2 / 3</div>
+  <img src="img_snow_wide.jpg" style="width:100%">
+  <div class="text">Caption Two</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">3 / 3</div>
+  <img src="img_mountains_wide.jpg" style="width:100%">
+  <div class="text">Caption Three</div>
+</div>
+
+
+
+</div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span> 
+  <span class="dot" onclick="currentSlide(2)"></span> 
+  <span class="dot" onclick="currentSlide(3)"></span> 
+</div>*/
