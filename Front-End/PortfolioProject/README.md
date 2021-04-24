39e10799df15f16de202406593e07ea50e9be7db
AhmedHajiyev <hajiyev.ahmed01@gmail.com>
2021-04-25 00:44:55 +0400

    blog hissesinde son isler

---
+++ Front-End/PortfolioProject/README.md
@@ -0,0 +1,2 @@
+Portfolio Project<br>
+https://preview.colorlib.com/theme/clark/index.html<hr>
--- Front-End/PortfolioProject/js/script.js
+++ Front-End/PortfolioProject/js/script.js
@@ -1,29 +1,3 @@
-/*let images = document.querySelectorAll('.slider');
-let start = 0;
-let dots = document.querySelectorAll('.dot')
-
-images[start].style.display = 'block'
-
-console.log(dots)
-
-const moveSlider = () => {
-    if (start = 1) {
-        start = 0;
-    };
-    for (let i = 0; i < images.length; i++) {
-        images[i].style.display = 'none'
-    }
-
-}
-
-
-
-setInterval(() => {
-    moveSlider();
-
-}, 2000)*/
-
-
 let slideIndex = 0;
 showSlides(slideIndex);
 
@@ -60,36 +34,3 @@ setInterval(() => {
     showSlides();
 
 }, 2000)
-
-console.log(showSlides)
-
-/*<div class="slideshow-container">
-
-<div class="mySlides fade">
-  <div class="numbertext">1 / 3</div>
-  <img src="img_nature_wide.jpg" style="width:100%">
-  <div class="text">Caption Text</div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">2 / 3</div>
-  <img src="img_snow_wide.jpg" style="width:100%">
-  <div class="text">Caption Two</div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">3 / 3</div>
-  <img src="img_mountains_wide.jpg" style="width:100%">
-  <div class="text">Caption Three</div>
-</div>
-
-
-
-</div>
-<br>
-
-<div style="text-align:center">
-  <span class="dot" onclick="currentSlide(1)"></span> 
-  <span class="dot" onclick="currentSlide(2)"></span> 
-  <span class="dot" onclick="currentSlide(3)"></span> 
-</div>*/
