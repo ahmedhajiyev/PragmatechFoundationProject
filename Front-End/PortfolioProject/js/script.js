2464f1dc22f43a5985a748da832c68969110368b
AhmedHajiyev <hajiyev.ahmed01@gmail.com>
2021-05-03 01:22:02 +0400

    slider js

--- Front-End/PortfolioProject/js/script.js
+++ Front-End/PortfolioProject/js/script.js
@@ -1,89 +1,72 @@
-/*let slideIndex = 0;
-showSlides(slideIndex);
+//For Slider
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
+let images = document.querySelectorAll('.slider');
+let start = 0;
+images[start].style.display = 'block';
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-
-
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("slider");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = n - 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+document.querySelector('.dot1').addEventListener('click', () => {
+    if (start == images.length - 1) {
+        start = -1;
+    };
+    for (let i = 0; i < images.length; i++) {
+        images[i].style.display = 'none';
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", " ");
+    images[++start].style.display = 'block';
+})
+
+
+document.querySelector('.dot2').addEventListener('click', () => {
+    start -= 1;
+    if (start < 0) {
+        start = images.length - 1;
     }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+    for (let i = 0; i < images.length; i++) {
+        images[i].style.display = 'none';
+    }
+    images[start].style.display = 'block';
+})
 
 
+
+const moveSlider = () => {
+    if (start == images.length - 1) {
+        start = -1;
+    };
+    for (let i = 0; i < images.length; i++) {
+        images[i].style.display = 'none';
+    }
+    images[++start].style.display = 'block';
 }
 
-setInterval(() => {
-    showSlides(slideIndex += 1);
 
-}, 2000)
-*/
-
+/* Sonsuz */
+let move = setInterval(() => {
+    moveSlider();
+}, 10000);
 
 //
 
-/*function myFunction() {
-    let i;
-    let index = document.getElementByClassName("index1");
-    index[i].classList.add("show");
 
-    alert();
-}*/
-
-/*function myFunction() {
-    document.getElementsByClassName("index1").style.opacity = "0.7 !important";
-}*/
-
-/*let move = 0;
-let sliderLine = document.querySelector('.slider');
-
-console.log(sliderLine)
-
-document.querySelector('.dot1').addEventListener('click', () => {
-    sliderLine.style.transition = 'all 0.8s ease 0s';
-    move += 291;
-    if (move > 873) {
-        sliderLine.style.transition = 'none';
-        move = 0
+const animationItems = document.querySelectorAll('._anim');
+const animationOnScroll = () => {
+    if (animationItems.length > 0) {
+        for (let i = 0; i < animationItems.length; i++) {
+            // Animasiya ne zaman baslasin
+            const animationStart = 4;
+            // ekranin olcusu elementin olsunun 1/4 oldugda animasi aktivlessin
+            let animationItemPoint = window.innerHeight - animationItems[i].offsetHeight / animationStart;
+            // Eger scroll deyeri > sehifenin en basindan elemente geder olan mesafe - elementin 1/4 geder
+            if ((pageYOffset > (animationItems[i].offsetTop - animationItemPoint)) && (pageYOffset < (animationItems[i].offsetTop + animationItems[i].offsetHeight))) {
+                animationItems[i].classList.add('_active');
+            } else {
+                // Animasiyani  tekrarlanmamasi ucun sert
+                if (!animationItems[i].classList.contains('_noRepeat')) {
+                    animationItems[i].classList.remove('_active');
+                }
+            }
+        }
     }
-    sliderLine.style.left = -move + 'px';
-})
-
-document.querySelector('.dot2').addEventListener('click', () => {
-    sliderLine.style.transition = 'all 0.8s ease 0s';
-    move -= 291;
-    if (move < 0) {
-        sliderLine.style.transition = 'none';
-        move = 873
-    }
-    sliderLine.style.left = -move + 'px';
-})*/
-
-
-let smooth = document.querySelectorAll('.smooth');
-for (let i = 0; i < smooth.length; i++) {
-    smooth[i].addEventListener('click', (e) => {
-        e.preventDefault();
-        const sectionId = e.target.getAttribute('href');
-        document.querySelector(sectionId).scrollIntoView({
-            behavior: 'smooth'
-        })
-    })
-
 }
+window.addEventListener('scroll', animationOnScroll);
+// Sehife acilanda il section animasiyanin aktivlesmesi
+animationOnScroll();
